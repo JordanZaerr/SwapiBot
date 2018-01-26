346633c2a44bd30a6dd9bbf2f7b4bff3314f63d7
Jordan Zaerr <jordan.zaerr@gmail.com>
2018-01-26 16:16:42 -0700

    Iniital code, default template

---
+++ src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=swapibot-Web-Deploy.pubxml -p:Password=z9jvTBMdrH5HHoEdCNgwZn91K5ZQhHCjTEWvvssTWjfZDmWkTSAh1QxzutHW
+
